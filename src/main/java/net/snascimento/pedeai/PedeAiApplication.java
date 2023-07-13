580df07a8b1581521b78bec0b33f3cc51178b38b
Andre S Nascimento <andrenascimento@luizalabs.com>
2023-07-13 12:08:48 -0300

    Ajuste de Variaveis de Ambiente

--- src/main/java/net/snascimento/pedeai/PedeAiApplication.java
+++ src/main/java/net/snascimento/pedeai/PedeAiApplication.java
@@ -2,8 +2,10 @@ package net.snascimento.pedeai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(DBConfigProperties.class)
 public class PedeAiApplication {
 
   public static void main(String[] args) {
